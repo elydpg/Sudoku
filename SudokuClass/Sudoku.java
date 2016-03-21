5438a178eed3ad865d601916fbc8ebd11ec5dfb6
elyisgreat <elyisgreat2@gmail.com>
2016-03-21 11:18:56 -0400

    added methods for accessing the fields

--- SudokuClass/Sudoku.java
+++ SudokuClass/Sudoku.java
@@ -8,8 +8,8 @@ public class Sudoku{
   private byte[] tiles;
   
   public Sudoku(byte[] tileset){
-    int size=(int)Math.pow(tileset.length,1/4);
-    if((int)Math.pow(size,4)!=tileset.length||size<MIN_SIZE||size>MAX_SIZE){throw new IllegalArgumentException(tileset.length+" is an  invalid number of tiles");}
+    int size=(int)Math.pow(tileset.length,1d/4d);
+    if((int)Math.pow(size,4)!=tileset.length||size<MIN_SIZE||size>MAX_SIZE){throw new IllegalArgumentException(tileset.length+" is an invalid number of tiles");}
     tiles=new byte[tileset.length];
     for(int c=0;c<tileset.length;c++){
       tiles[c]=tileset[c];
@@ -17,6 +17,18 @@ public class Sudoku{
   }
   public static Sudoku copy(final Sudoku obj){
     return new Sudoku(obj.tiles);
+  }
+  
+  public byte[] getTiles(){
+    byte[] tileset=new byte[tiles.length];
+    for(int c=0;c<tiles.length;c++){
+      tileset[c]=tiles[c];
+    }
+    return tileset;
+  }
+  
+  public int getSize(){
+    return (int)Math.pow(tiles.length,1d/4d);
   }
   
   public static void main(String[] args){
