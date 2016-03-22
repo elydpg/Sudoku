8634c76bebcc182fa206a35634d983d1abc322c9
elyisgreat <elyisgreat2@gmail.com>
2016-03-22 16:05:11 -0400

    Validity methods
    -added equals and hashcode
    -added methods to check for valid sudoku, solved sudoku, and a
    perfectly solved sudoku (solved and valid)

--- SudokuClass/Sudoku 2.java
+++ SudokuClass/Sudoku 2.java
@@ -7,27 +7,6 @@ public class Sudoku{
   
   private byte[] tiles;
   
-    private static byte[] removeElementVal(byte[] a,byte elementVal){
-    byte[] b=new byte[0];
-    for(int c=0;c<a.length;c++){
-      if(a[c]!=elementVal){
-        b=Arrays.copyOf(b,b.length+1);
-        b[b.length-1]=a[c];
-      }
-    }
-    return b;
-  }
-  
-    private static boolean allEmementsUnique(byte[] b){
-      if(b.length>256){return false;}
-      for(int i = 0; i < b.length-1;i++){
-        for(int j=i+1;j<b.length;j++){
-          if(b[i]==b[j]){return false;}
-          }
-         }              
-    return true;   
-    }
-  
   public Sudoku(byte[] tileset){
     int size=(int)Math.pow(tileset.length,1d/4d);
     if((int)Math.pow(size,4)!=tileset.length||size<MIN_SIZE||size>MAX_SIZE){throw new IllegalArgumentException(tileset.length+" is an invalid number of tiles");}
@@ -39,17 +18,8 @@ public class Sudoku{
       tiles[c]=tileset[c];
     }
   }
-  
   public static Sudoku copy(final Sudoku obj){
     return new Sudoku(obj.tiles);
-  }
-  
-  public boolean equals(Object obj){
-    return Arrays.equals(tiles,((Sudoku)obj).tiles);
-  }
-  
-  public int hashCode(){
-    return Arrays.hashCode(tiles);
   }
   
   public byte[] getTiles(){
@@ -74,21 +44,6 @@ public class Sudoku{
   
   public byte getTileInBox(int box,int pos){
     return tiles[getSize()*(box/getSize())*getSize()*getSize()+getSize()*(box%getSize())+getSize()*getSize()*(pos/getSize())+(pos%getSize())];
-  }
-  
-  public void setTile(int tileNumber,byte tileValue){
-    if(tileValue<-1||tileValue>=(getSize()*getSize())){throw new IllegalArgumentException("Tile "+tileValue+" has an invalid value");}
-    tiles[tileNumber]=tileValue;
-  }
-  
-  public void setTile(int row,int column,byte tileValue){
-    if(tileValue<-1||tileValue>=(getSize()*getSize())){throw new IllegalArgumentException("Tile "+tileValue+" has an invalid value");}
-    tiles[getSize()*getSize()*row+column]=tileValue;
-  }
-  
-  public void setTileInBox(int box,int pos,byte tileValue){
-   if(tileValue<-1||tileValue>=(getSize()*getSize())){throw new IllegalArgumentException("Tile "+tileValue+" has an invalid value");}
-   tiles[getSize()*(box/getSize())*getSize()*getSize()+getSize()*(box%getSize())+getSize()*getSize()*(pos/getSize())+(pos%getSize())]=tileValue;
   }
   
   public byte[] getRow(int row){
@@ -116,21 +71,6 @@ public class Sudoku{
       boxTiles[c]=tiles[rowOffset*getSize()*getSize()+columnOffset+getSize()*getSize()*(c/getSize())+(c%getSize())];
     }
     return boxTiles;
-  }
-  
-  public boolean isSolvedSudoku(){
-    return tiles.length==removeElementVal(tiles,(byte)-1).length;
-  }
-  
-  public boolean isValidSudoku(){
-    for(int c=0;c<getSize()*getSize();c++){
-      if((allEmementsUnique(getRow(c))&&allEmementsUnique(getColumn(c))&&allEmementsUnique(getBox(c)))==false){return false;}
-    }
-    return true;
-  }
-  
-  public boolean isValidSolvedSudoku(){
-    return isSolvedSudoku()&&isValidSudoku();
   }
   
   public static void main(String[] args){
